56dbeb9c2e262718834fccb3bc65c1747120e0a8
kevincoleman <kevin.n.coleman@gmail.com>
2016-07-26 14:55:32 -0700

    Renamed critter service

---
+++ src/scripts/services/critter.service.ts
@@ -0,0 +1,13 @@
+// Core
+import { Injectable } from "@angular/core";
+
+import { Critter } from "../models";
+
+@Injectable()
+export class CritterService {
+
+  // public critters = [
+
+  // ];
+
+}
