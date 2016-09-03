5df5e300a6c676b6f833d93c221b63ec034f155e
kevincoleman <kevin.n.coleman@gmail.com>
2016-09-02 22:37:54 -0700

    added start of critter service

--- src/scripts/services/critter.service.ts
+++ src/scripts/services/critter.service.ts
@@ -1,11 +1,68 @@
 // Core
-import { Injectable } from "@angular/core";
+import { Injectable }
+  from "@angular/core";
+
+import { MapService }
+  from "./map.service";
+
+import { Critter }
+  from "../models";
 
 @Injectable()
 export class CritterService {
 
-  // public critters = [
+  public critters: Critter[];
 
-  // ];
+  constructor(
+    private mapService: MapService
+  ) { }
 
+  public go(critter, direction) {
+    switch (direction) {
+      case "up":
+        if (
+          this.mapService.isNavigable(
+            this.mapService.getSpace(
+              critter.positionX,
+              critter.positionY - 1,
+              this.mapService.nearby
+            )
+          )
+        ) { critter.positionY--; }
+        break;
+      case "down":
+        if (
+          this.mapService.isNavigable(
+            this.mapService.getSpace(
+              critter.positionX,
+              critter.positionY + 1,
+              this.mapService.nearby
+            )
+          )
+        ) { critter.positionY++; }
+        break;
+      case "left":
+        if (
+          this.mapService.isNavigable(
+            this.mapService.getSpace(
+              critter.positionX - 1,
+              critter.positionY,
+              this.mapService.nearby
+            )
+          )
+        ) { critter.positionX--; }
+        break;
+      case "right":
+        if (
+          this.mapService.isNavigable(
+            this.mapService.getSpace(
+              critter.positionX + 1,
+              critter.positionY,
+              this.mapService.nearby
+            )
+          )
+        ) { critter.positionX++; }
+        break;
+    }
+  }
 }
